dd9d898eb9059ab697ecc4da85b9c9c48a867867
Selvyn <swright@celestial.co.uk>
2021-01-30 23:49:11 +0000

    HTML pages with scripts, two versions to calculate age

---
+++ test/personal_details.spec.ts
@@ -0,0 +1,15 @@
+import {Selector} from "testcafe";
+import sleep from "../src/sleep_utils"
+
+
+fixture `Personal Details Tests`            // Notice the back-tick, NOT a single or double quote
+    .page `../src/practice_page.html`     // notice what heppens when the test is run...  Add semicolon if no optional elements
+
+    test('test username', async t => {
+        const username = Selector('#username');
+
+        await t     // We wait on a Promise
+            .typeText(username, 'species8472')
+            .expect(username.value).contains('species8472', 'input contains the test "species8472"')
+        await sleep(3000);
+    });
---
+++ test/simple_test.spec.ts
@@ -0,0 +1,21 @@
+import {Selector} from "testcafe";
+import sleep from "../src/sleep_utils"
+
+fixture `Simple test`            // Notice the back-tick, NOT a single or double quote
+    .page `../src/simple_page.html`     
+    
+    test('Test1', async t => {  // The async is important, calls to pages are across a network
+        const msg = Selector('#message');
+        
+        await t     // We wait on a Promise
+            .typeText(msg, 'Hello from TestCafe')
+            .expect(msg.value).contains('Hello', 'input contains the test "Hello"')
+
+        // comment out the sleep statements to see the code run in realtime
+        await sleep(3000);
+        await t
+            .click("#submit-button")
+            .expect(msg.value).contains('', 'input is empty');
+
+        await sleep(3000);
+     });
